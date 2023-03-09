b35f5d2622de592e76b6b9478b726f6841b0e451
Isadora Stangarlin <isadorastangarlin@MacBook-Air-de-Isadora.local>
2023-03-09 10:17:44 -0300

    user sign up errors

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,4 +2,17 @@ class RegistrationsController < ApplicationController
     def new
         @user = User.new
     end
+
+    def create
+      @user = User.new(user_params)  
+      if @user.save
+        redirect_to root_path, notice: "Successfully created account"
+      else 
+        render :new, status: 422
+      end 
+    end
+
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 end
