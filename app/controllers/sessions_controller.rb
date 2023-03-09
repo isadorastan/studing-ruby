6c51fe1c874df47f2b945d09965267237f77521e
Isadora Stangarlin <isadorastangarlin@MacBook-Air-de-Isadora.local>
2023-03-09 10:44:22 -0300

    user sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
+    def new 
+    end
+
+    def create
+        user = User.find_by(email: params[:email])
+        if user.present? && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to root_path, notice: "Logged In"
+        else
+            flash[:alert] = "Invalid email or password"
+            render :new, status: 422
+        end
+    end
+    
     def destroy
         session[:user_id] = nil
         redirect_to root_path, notice: "Logged Out"
