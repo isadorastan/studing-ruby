8be8d4fc501b58bba8438024d0e0e53c4dc8f10c
Isadora Stangarlin <isadorastangarlin@MacBook-Air-de-Isadora.local>
2023-03-12 18:41:19 -0300

    Update password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,6 @@
 class PasswordResetsController < ApplicationController
-def new
-end
+    def new
+    end
 
     def create
         @user = User.find_by(email: params[:email])
@@ -13,7 +13,22 @@ end
         redirect_to root_path, notice: "If an account with this email was found, we have sent a link to reset your password"
     end
 
+    def edit 
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
+    rescue ActiveSupport::MessageVerifier::InvalidSignature
+        redirect_to sign_in_path, alert: "Your token has expired. Please try again"
+    end
+
+    def update
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
+        if @user.update(password_params)
+            redirect_to sign_in_path, notice: "Your password was reset successfully. Please sign in"
+        else
+            render :edit, status: :bad_request
+        end
+    end
+
+    def password_params
+        params.required(:user).permit(:password, :password_confirmation)
+    end 
 end
-
-
-# rails g mailer Password reset
